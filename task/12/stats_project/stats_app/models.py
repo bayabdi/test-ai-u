88e136a656922e52aa9a25e26eee8da1e808a592
BaYeL AbDiKaRiMoV <abdikarimov.bayel@gmail.com>
2023-12-22 18:22:49 +0600

    12 django init

--- task/12/stats_project/stats_app/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class Statistic(models.Model):
-    timestamp = models.DateTimeField()
-    value = models.FloatField()
--- task/12/stats_project/stats_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import statistics_endpoint
-
-urlpatterns = [
-    path('api/statistics/', statistics_endpoint, name='statistics_endpoint'),
-]
